d70b4b9bbeef8324c42d06dfd5266f275e875afc
Luke Wynn <LWynn2@my.gcu.edu>
2020-01-10 14:36:24 -0700

    final

---
+++ In Class Activity 1.8.20/Source.cpp
@@ -0,0 +1,15 @@
+//Luke Wynn
+//This is my own work
+//In class exercise 1/8/20
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float kilogram, pound;
+	cout << "Enter Pound";
+	cin >> pound;
+	kilogram = pound * .454;
+	cout << pound << "pound =" << kilogram << " kg";
+	return 0;
+}
